1fe591de258be8937c41a7f3031e2f65043130e1
Pure <1356382998@qq.com>
2024-12-11 11:35:13 +0800

    选购页面完成

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
+import {ref} from 'vue'
 //接收跳转页面参数
 onLoad((options) => {
   const id = options.id
@@ -7,12 +8,46 @@ onLoad((options) => {
   // 根据 ID 获取商品详情逻辑
 })
 
+//返回上一页
 const goBack = () => {
   uni.navigateBack()
 }
+
+//购物车跳转
+const goCart = () => {
+  uni.switchTab({
+    url: '/pages/cart/cart',
+  })
+}
+
+//用来显示租车方案
+const showRentPrice = () => {
+  isShow.value = true;
+}
+
+//关闭租车方案
+const close = () => {
+  isShow.value = false;
+}
+//加减天数
+const add = () => {
+  rentDays.value += 1;
+}
+const sub = () => {
+  if(rentDays.value > 0) {
+    rentDays.value -= 1;
+  }
+}
 //api
 
+//状态映射
 const statusMap = ['正常', '待提车', '租赁中', '已售', '待归还']
+//是否显示租车方案
+const isShow = ref(false);
+//租车天数
+const rentDays = ref(0);
+//租车方案选择   0-天  1-月
+const rentChoose = ref(0);
 
 //模拟数据
 const data = {
@@ -51,10 +86,12 @@ const data = {
       </view>
       <!-- 名字、编号、车况、车龄 -->
       <view class="information-bottom">
-        <text class="name" style="margin-bottom: 5rpx;">{{ data.name }}</text>
-        <text class="number" style="margin-bottom: 5rpx;">单车编号: {{ data.number }}</text>
-        <text class="description" style="margin-bottom: 5rpx;">车况介绍: {{ data.description }}</text>
-        <text class="age" style="margin-bottom: 5rpx;">车龄: {{ data.age }}年</text>
+        <text class="name" style="margin-bottom: 5rpx">{{ data.name }}</text>
+        <text class="number" style="margin-bottom: 5rpx">单车编号: {{ data.number }}</text>
+        <text class="description" style="margin-bottom: 5rpx"
+          >车况介绍: {{ data.description }}</text
+        >
+        <text class="age" style="margin-bottom: 5rpx">车龄: {{ data.age }}年</text>
       </view>
     </view>
 
@@ -73,15 +110,17 @@ const data = {
     <view class="bottom">
       <view class="bottom-title">
         <text class="attention">云百客交易须知</text>
-        <navigator class="detail">付款前了解交易规则，保障您的权益 <text class="icon-right"></text></navigator>
+        <navigator class="detail"
+          >付款前了解交易规则，保障您的权益 <text class="icon-right"></text
+        ></navigator>
       </view>
 
       <view class="price-information">
-        <navigator class="tobuy" url="/pages/cart/cart">
+        <view class="tobuy" @tap="goCart">
           <image src="@/static/tabs/cart_default.png" class="tobuy-image"></image>
-        </navigator>
+        </view>
         <text class="tag">租越久，越便宜</text>
-        <view class="price-daily-box">
+        <view class="price-daily-box" @tap="showRentPrice">
           <text class="price-daily">{{ data.daily }}币/天</text>
           <text class="price-tag">定期租赁</text>
         </view>
@@ -90,6 +129,35 @@ const data = {
           <text class="sell-tag">全款购买</text>
         </view>
       </view>
+    </view>
+  </view>
+  <!-- 租车方案 -->
+  <view
+    v-if="isShow"
+    class="rent-detail"
+  >
+    <view class="rent-detail-title">
+      选择租车方案
+      <text class="close" @tap="close">x</text>
+    </view>
+    <view class="rent-detail-content">
+      <view class="detail-daily-box" @tap="rentChoose = 0">
+        <text class="detail-daily-title">日租</text>
+        <text class="detail-daily-price">{{ data.daily }}币/天</text>
+      </view>
+      <view class="detail-monthly-box">
+        <text class="detail-monthly-title" @tap="rentChoose = 1">月租</text>
+        <text class="detail-monthly-price">{{ data.monthly }}币/月</text>
+      </view>
+    </view>
+    <view class="rent-detail-bottom">
+      <text class="rent-tag">租车时长</text>
+      <text class="rent-time">共{{ rentDays }}{{ rentChoose === 0 ? '天' : '月' }}</text>
+      <text class="rent-options">
+        <text class="time-sub" @tap="sub">-</text>
+        <text class="rent-option-time">{{ rentDays }}</text>
+        <text class="time-add" @tap="add">+</text>
+      </text>
     </view>
   </view>
 </template>
@@ -167,53 +235,183 @@ page {
       }
     }
   }
-  .user{
+  .user {
     display: flex;
     flex-direction: column;
     background: #fff;
     border-radius: 20rpx;
     margin: 5rpx 10rpx 20rpx;
-    .safe{
+    .safe {
       margin: 25rpx 20rpx 0;
       padding-bottom: 25rpx;
       border-bottom: 2rpx solid #e5e5e5;
-      .safe-title{
+      .safe-title {
         color: #7f7f7f;
-        margin-right: 100rpx
+        margin-right: 100rpx;
       }
     }
-    .address{
+    .address {
       margin: 0 20rpx 25rpx;
       padding-top: 25rpx;
-      .address-title{
+      .address-title {
         color: #7f7f7f;
-        margin-right: 100rpx
+        margin-right: 100rpx;
       }
     }
   }
 
-  .bottom{
+  .bottom {
     display: flex;
     flex-direction: column;
     background: #fff;
     border-radius: 20rpx;
     margin: 5rpx 10rpx 20rpx;
-    .bottom-title{
+    .bottom-title {
       display: flex;
       flex-direction: row;
       margin-left: 20rpx;
     }
-    .detail{
+    .detail {
       margin-left: 20rpx;
       color: #7f7f7f;
     }
-    .price-information{
+    .price-information {
       display: flex;
       flex-direction: row;
       color: #cf4444;
-      .tobuy-image{
-        width: 50rpx;
-        height: 50rpx;
+      margin-top: 50rpx;
+      .tobuy {
+        margin-left: 15rpx;
+        .tobuy-image {
+          width: 80rpx;
+          height: 80rpx;
+        }
+      }
+      .tag {
+        font-size: 20rpx;
+        margin: 40rpx 10rpx 0 80rpx;
+      }
+      .price-daily-box{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: #000;
+        margin-bottom: 20rpx;
+        width: 200rpx;
+        height: 100rpx;
+        border-radius: 20rpx 0 0 20rpx;
+        background-color: #7f7f7f;
+      }
+      .price-sell-box{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 20rpx;
+        width: 200rpx;
+        height: 100rpx;
+        color: #fff;
+        border-radius: 0 20rpx 20rpx 0;
+        background-color: #cf4444;
+      }
+    }
+  }
+}
+
+.rent-detail{
+  position: absolute;
+  z-index: 999;
+  height: 30vh;
+  width: 97.5vw;
+  bottom: 0;
+  margin: 0 10rpx;
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  border-radius: 20rpx 20rpx 0 0;
+  .rent-detail-title{
+    display: flex;
+    flex-direction: row;
+    margin: 20rpx 0 0 20rpx;
+    font-size: 35rpx;
+    font-weight: bold;
+    .close{
+      margin-left: 450rpx;
+    }
+  }
+  .rent-detail-content{
+    display: flex;
+    flex-direction: row;
+    margin: 20rpx 0 0 20rpx;
+    .detail-daily-box{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-weight: bold;
+      background-color: #7f7f7f;
+      border-radius: 20rpx;
+      margin-right: 150rpx;
+      height:250rpx;
+      width:250rpx;
+      .detail-daily-title{
+        color:#fff;
+        margin-bottom:20rpx;
+      }
+      .detail-daily-price{
+        font-size: 45rpx;
+        color: #cf4444;
+      }
+    }
+    .detail-monthly-box{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-weight: bold;
+      background-color: #7f7f7f;
+      border-radius: 20rpx;
+      height:250rpx;
+      width:250rpx;
+    }
+    .detail-monthly-title{
+      color:#fff;
+      margin-bottom:20rpx;
+    }
+    .detail-monthly-price{
+      font-size: 45rpx;
+      color: #cf4444;
+    }
+  }
+  .rent-detail-bottom{
+    display: flex;
+    flex-direction: row;
+    margin: 20rpx 0 0 20rpx;
+    .rent-time{
+      margin-left: 300rpx;
+    }
+    .rent-options{
+      margin-left: 20rpx;
+      display: flex;
+      .time-sub{
+        font-size:30rpx;
+        font-weight: bold;
+        border: 2rpx solid #7f7f7f;
+        border-radius: 10rpx;
+        width: 40rpx;
+        height: 10rpx;
+      }
+      .time-add{
+        font-size:30rpx;
+        font-weight: bold;
+        border: 2rpx solid #7f7f7f;
+        border-radius: 10rpx;
+        width: 40rpx;
+        height: 10rpx;
+      }
+      .rent-option-time{
+        margin: 0 10rpx;
       }
     }
   }
